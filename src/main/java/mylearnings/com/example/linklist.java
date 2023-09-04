e48ab571e16b7baa6a806058912556a0a1db9e22
Dhanashri <dhanashri.patkar@siliconveins.com>
2023-09-04 07:40:43 +0530

    Initial commit

---
+++ src/main/java/mylearnings/com/example/linklist.java
@@ -0,0 +1,13 @@
+package mylearnings.com.example;
+
+public class linklist {
+
+    static void print() {
+        System.out.println("Test java");
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+
+}
